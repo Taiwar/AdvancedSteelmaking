c8ce8dfb519bbfbc56776b85d1c67da0c5fe1622
Taiwar <thenixchecker@gmail.com>
2017-06-07 17:57:39 +0200

    Basic setup

---
+++ src/main/java/net/muellersites/advancedsteelmaking/AdvancedSteelmaking.java
@@ -0,0 +1,29 @@
+package net.muellersites.advancedsteelmaking;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="AdvancedSteelmaking", name="Advanced Steelmaking", version="1.10.2-1.0")
+public class AdvancedSteelmaking {
+
+    @Mod.Instance("AdvancedSteelmaking")
+    public static AdvancedSteelmaking instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+
+}
